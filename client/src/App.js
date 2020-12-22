8bdaf23813d58228ebc8d7cf6699ba742499233c
Ankit <ankit.escalate360@gmail.com>
2020-12-22 23:39:47 +0530

    client

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "semantic-ui-react";
+
+import "semantic-ui-css/semantic.min.css";
+import "./App.css";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+
+import MenuBar from "./component/MenuBar";
+
+function App() {
+  return (
+    <Router>
+      <Container>
+        <MenuBar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Container>
+    </Router>
+  );
+}
+
+export default App;
